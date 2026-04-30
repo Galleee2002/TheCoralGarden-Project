d225007ce8bee7e4f2c4f41497fec1ddbe26d533
Galle <gaelgarcia.dev@gmail.com>
2026-04-30 19:34:36 -0300

    fix: resolve navbar lint after merge

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -43,10 +43,6 @@ export function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [isTransparentPage]);
 
-  useEffect(() => {
-    setMobileOpen(false);
-  }, [pathname]);
-
   return (
     <>
       <header
